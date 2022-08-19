9a4dc381a3143ceb9752e6adc357ac254219a842
shuyu <shuyu.dong@m4x.org>
2022-08-19 07:24:19 +0200

    [exp2]

--- exp_2f.py
+++ exp_2f.py
@@ -12,49 +12,32 @@ from external.test_linear import notears_linear
 import ges
 
 
-def test_save_res_icid(W_true, X, k=25, \
-                       optp1=2e-1, optp2=1e-1, \
-                       tid=None, fdir='outputs/exp0_'):
-    if not os.path.exists(fdir):
-        os.makedirs(fdir)
-    """ ICID """
-    print('-------Ready to run ICID ----------')
-    W_icd, ith_icd  = run_icid(X, sigma_0=1.0, k=k,\
-                              lambda_1=optp1, \
-                              idec_lambda1=optp2, \
-                              beta_2 = 0.7, gamma_2=1.0, \
-                              maxit_prox_inner=500, W_true=W_true)
-    """ Evaluations and record results """
-    acc = utils.count_accuracy(W_true!=0, W_icd!=0)
-    """ SAVE RESULTS """
-    pd.DataFrame(ith_icd).to_csv('%s/%s_ith_icid.csv'\
-                                    %(fdir, tid))
-    return acc, ith_icd
-
 if __name__ == '__main__':
     timestr = time.strftime("%H%M%S%m%d")
-    FDIR = 'outputs/exp2_%s' % timestr
-    if not os.path.exists(FDIR):
-        os.makedirs(FDIR)
+    FDIR = 'outputs/exp2f_%s' % timestr
     # Generate input parameters
-    ds            = np.array([25,50,75,100,200,300,400])
-    degs          = np.linspace(1.2,2.0,5) # 0.5
-    graph_types   = ['ER']
-    sem_types     = ['gauss']
-    R_N2D         = 10
-    # n = 8*ds  # to be updated later (depending on d) on the fly
+    ds            = np.array([50,200]) #x5  #,300,400,500])
+    degs          = [1] #[4, 2, 1] #x3 # 0.5
+    graph_types   = ['ER'] #'SF']
+    sem_types     = ['gauss'] #['gauss','exp'] # x2
+    R_N2D         = 32
     #
     pbs_l, pbs = gen_list_settings(d=ds, degs=degs, \
                                  graph_types=graph_types, \
                                  sem_types=sem_types)
-
     # Methods to run
+    #
     ms = {'icid':   False, \
           'ges':    False,\
-          'notears':False}
+          'notears':False,\
+          'ideal':  False}
     ni = len(sys.argv)
     for i in range(ni):
         ms[sys.argv[i]] = True
+    if ni > 1:
+        FDIR = 'outputs/exp2f_%s_%s' % (timestr, sys.argv[1])
+    if not os.path.exists(FDIR):
+        os.makedirs(FDIR)
     # Iterate through all problem settings ('pbs')
     res = []
     for i in range(len(pbs)):
@@ -69,23 +52,43 @@ if __name__ == '__main__':
                                 sem_type    = pbs['sem_type'][i], \
                                 seed = 1)
         #----------- ICID ------------
-        if ms['icid']:
+        if ms['icid'] or ms['ideal']:
+            if ms['ideal']:
+                IC = 'ideal'
+            else:
+                IC = 'emp_gs'
             # Parameters of ICID algorithm
-            opts={'k':              [25], \
-                  'lambda_1':       [1e-1, 4e-1], \
-                  'idec_lambda1':   [1e-1]}
+            opts={'opt_ic':         [IC], \
+                'idec_solver':     ['FISTA'], \
+                  'k':              [25], \
+                  'lambda_1':       [5e-2], \
+                  'idec_lambda1':   [1e-2, 4e-2, 5e-2]}  #
             l_o, df_o = gen_list_optparams(opts)
             print('List of opt parameters to tets are:')
             print(df_o)
             # Iterate through all optimization parameter configs
             for j in range(len(df_o)):
-                acc, ith = test_save_res_icid(Wtrue, X, \
-                                    k    =df_o['k'][j], \
-                                    optp1=df_o['lambda_1'][j] , \
-                                    optp2=df_o['idec_lambda1'][j] , \
-                                    tid='pb%dopt%d' %(i+1,j+1), \
-                                    fdir=FDIR)
+                print('Opt parameter to run now:')
+                print(df_o.iloc[j])
+                if pbs['deg'][i] > 2:
+                    lambda1s = np.linspace(1e-2,1e-1,10)
+                else:
+                    lambda1s = df_o['lambda_1'][j]
+                W_icd, ith, idh  = run_icid(X, sigma_0=1.0, \
+                                          k=df_o['k'][j], \
+                                          lambda_1= lambda1s, \
+                                          idec_lambda1=df_o['idec_lambda1'][j],\
+                                          beta_2 = 0.7, gamma_2=5.0, \
+                                          opt_ic=df_o['opt_ic'][j], \
+                                          idec_solver = df_o['idec_solver'][j], \
+                                          maxit_prox_inner=500, W_true=Wtrue)
+                """ Evaluations and record results """
+                acc = utils.count_accuracy(Wtrue!=0, W_icd!=0)
+                """ SAVE RESULTS """
+                pd.DataFrame(ith).to_csv('%s/pb%dopt%d_ith_icid.csv' %(FDIR, i+1,j+1))
+                pd.DataFrame(idh).to_csv('%s/pb%dopt%d_ith_idec.csv' %(FDIR, i+1,j+1))
                 print(acc)
+                lambda_1 = ith[ith['niter']==-1].iloc[0]['subpb_name']
                 res.append({'alg':'ICID',
                             'd'           : pbs['d'][i], \
                             'deg'         : pbs['deg'][i], \
@@ -93,7 +96,9 @@ if __name__ == '__main__':
                             'graph_type'  : pbs['graph_type'][i],\
                             'sem_type'    : pbs['sem_type'][i], \
                             'k'           : df_o['k'][j], \
-                            'lambda_1'    : df_o['lambda_1'][j], \
+                            'ic'           : df_o['opt_ic'][j], \
+                            'id'           : df_o['idec_solver'][j], \
+                            'lambda_1'    : lambda_1, \
                         'idec_lambda1'    : df_o['idec_lambda1'][j], \
                         'shd':acc['shd'], 'tpr':acc['tpr'], \
                         'fdr':acc['fdr'], 'fpr':acc['fpr'], \
@@ -102,6 +107,8 @@ if __name__ == '__main__':
                 pd.DataFrame(res, columns=res[0].keys()).to_csv('%s/res_all.csv' %FDIR)
         #-------- NOTEARS ----------------
         if ms['notears']:
+            Xn = X[:10*pbs['d'][i], :]
+            print('For Notears: n = 10d (%d)' %Xn.shape[0])
             # Parameters
             opts={'lambda_1':       [1e-1, 4e-1]}
             l_o, df_o = gen_list_optparams(opts)
@@ -111,8 +118,8 @@ if __name__ == '__main__':
             for j in range(len(df_o)):
                 print('-------Ready to run NOTEARS ----------')
                 t0 = timer()
-                # w_notears, _ = NOTEARS
-                W_no, ith_no = notears_linear(X, \
+                #
+                W_no, ith_no = notears_linear(Xn, \
                                         lambda1=df_o['lambda_1'][j], \
                                         loss_type='l2', Wtrue=Wtrue)
                 t_no = timer() - t0
@@ -153,5 +160,6 @@ if __name__ == '__main__':
     if len(res) > 0:
         endstr = time.strftime("%H%M%S%m%d")
         pd.DataFrame(res, columns=res[0].keys()).to_csv('%s/resall_%s.csv' %(FDIR,endstr))
+
 
 
